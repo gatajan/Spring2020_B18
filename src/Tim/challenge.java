bc580db728fc80d5d41e6ad703fb0600b4a3d3f4
AG <gannaly@yahoo.com>
2020-04-23 16:41:19 -0500

    update

--- src/Tim/challenge.java
+++ src/Tim/challenge.java
@@ -1,7 +1,31 @@
 package Tim;
 
+import java.text.DateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 public class challenge {
     public static void main(String[] args) {
+
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+        LocalDate birthday = LocalDate.of(1985, 8, 8);
+        System.out.println(birthday);
+        LocalDateTime tom = LocalDateTime.now();
+        System.out.println(tom);
+
+        boolean b = birthday.isBefore(now);
+        System.out.println(b);
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MMM/dd/yy");
+        System.out.println(dtf.format(now));
+        System.out.println(dtf.format(birthday));
+
+
+
         double d1 = 20.0;
         double d2 = 80.0;
         double result = (d1 + d2)*100;
@@ -16,6 +40,8 @@ public class challenge {
         if (!b1) {
             System.out.println("Got some remiander");
             System.out.println("hello");
+
+
         }
 
     }
